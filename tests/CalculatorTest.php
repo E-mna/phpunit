1387038198f23ba4c041880889616048f522628f
emna <emnabenhammamia@gmail.com>
2024-07-01 17:18:39 +0200

    3 test ✅

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,4 +22,33 @@ class CalculatorTest extends TestCase
             "La méthode sum() ne fonctionne plus :-("
         );
     }
+
+    public function TestMultiplication():void{
+        //Arrange
+        $firstValue = 3;
+        $secondValue = 2;
+        $expectedResult = 13;
+
+        //Act
+        $calculator = new Calculator();
+        $multiplication = $calculator->multiplication($firstValue, $secondValue);
+
+        //Assert
+        $this->assertSame($expectedResult, $multiplication,  "La méthode multiplication() ne fonctionne plus :-(" );
+    }
+
+    public function testIsEven(): void{
+        //Arrange
+        $numberA = 2;
+        $numberB = 3;
+        //Act
+        $calculator = new Calculator();
+        $isResultA= $calculator->isEven($numberA);
+        $isResultB= $calculator->isEven($numberB);
+ 
+        //Assert
+        $this->assertTrue($isResultA, "La méthode isEven() devrait retourner true pour un nombre pair");
+        $this->assertFalse($isResultB, "La méthode isEven() devrait retourner false pour un nombre impair");
+
+    }
 }
